fea5b9f744179bc22fdfdb9bd0c1bff582452d52
stcksmsh <vukicevickosta@gmail.com>
2022-11-23 20:34:00 +0100

    Made some changes to the css, and added color picker as a project

--- javascript.js
+++ javascript.js
@@ -1,7 +1,29 @@
-/**
- * Description
- * @authors Your Name (you@example.org)
- * @date    2022-11-21 15:21:00
- * @version 1.0.0
- */
+const skills = ['C', 'C++', 'Python', 'TensorFlow', 'Java', 'JavaScript', 'CSS3', 'HTML5', 'React']
 
+const skillWrapper = document.getElementById('skills')
+let rows = (skills.length+2)/3;
+skillWrapper.style.gridTemplateRows = 'repeat(' + rows.toString() + ', 1fr)';
+// console.log('repeat(\${columns}, 1fr)');
+
+for (var i = 0; i < skills.length; i++){
+    let div = document.createElement('div');
+    div.classList = ['skill'];
+    let img = document.createElement('img');
+    img.classList = ['skill-icon'];
+    img.src = './images/' + skills[i] + '.png';
+    div.appendChild(img);
+    // div.textContent = skills[i];
+    div.innerHTML += skills[i];
+    skillWrapper.appendChild(div);
+}
+
+;
+document.getElementsByName('colorpicker1')[0].addEventListener("change", (event)=>{document.documentElement.style.setProperty('--color1', event.target.value);})
+
+document.getElementsByName('colorpicker2')[0].addEventListener("change", (event)=>{document.documentElement.style.setProperty('--color2', event.target.value);});
+
+document.getElementsByName('colorpicker3')[0].addEventListener("change", (event)=>{document.documentElement.style.setProperty('--color3', event.target.value);});
+
+document.getElementsByName('colorpicker4')[0].addEventListener("change", (event)=>{document.documentElement.style.setProperty('--color4', event.target.value);});
+
+document.getElementsByName('timepicker')[0].addEventListener("change", (event) => {document.documentElement.style.setProperty('--time', event.target.value + 's');})
