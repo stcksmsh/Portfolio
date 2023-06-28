02d6efd8da93e088197402875d9a149f0c0a4158
stcksmsh <vukicevickosta@gmail.com>
2023-06-28 03:32:57 +0200

    Changed projects to a slideshow removed color changing

--- javascript.js
+++ javascript.js
@@ -15,13 +15,29 @@ for (var i = 0; i < skills.length; i++){
     skillWrapper.appendChild(div);
 }
 
-;
-document.getElementsByName('colorpicker1')[0].addEventListener("change", (event)=>{document.documentElement.style.setProperty('--color1', event.target.value);})
+let slideIndex = 1;
+showSlides(slideIndex);
 
-document.getElementsByName('colorpicker2')[0].addEventListener("change", (event)=>{document.documentElement.style.setProperty('--color2', event.target.value);});
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-document.getElementsByName('colorpicker3')[0].addEventListener("change", (event)=>{document.documentElement.style.setProperty('--color3', event.target.value);});
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-document.getElementsByName('colorpicker4')[0].addEventListener("change", (event)=>{document.documentElement.style.setProperty('--color4', event.target.value);});
-
-document.getElementsByName('timepicker')[0].addEventListener("change", (event) => {document.documentElement.style.setProperty('--time', 40 - event.target.value + 's');})
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
