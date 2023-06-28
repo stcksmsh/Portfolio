26496e6173480af63e53ab19309f399dbfba5950
stcksmsh <vukicevickosta@gmail.com>
2023-06-28 05:11:16 +0200

    Added effects to the slideshow
    this took way too long

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 const skills = ['C', 'C++', 'Python', 'TensorFlow', 'Java', 'JavaScript', 'RISC-V', 'NodeJS', 'React']
 
 const skillWrapper = document.getElementById('skills')
-let rows = (skills.length+2)/3;
+let rows = (skills.length + 2) / 3;
 skillWrapper.style.gridTemplateRows = 'repeat(' + rows.toString() + ', 1fr)';
 
-for (var i = 0; i < skills.length; i++){
+for (var i = 0; i < skills.length; i++) {
     let div = document.createElement('div');
     div.classList = ['skill'];
     let img = document.createElement('img');
@@ -16,28 +16,35 @@ for (var i = 0; i < skills.length; i++){
 }
 
 let slideIndex = 1;
-showSlides(slideIndex);
+showSlides(1);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex + n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    slides[slideIndex - 1].getElementsByClassName("slideshow-image")[0].style.animation = "rotateOut 2s 1 ease forwards"
+    slides[slideIndex - 1].style.animation = "outRight 2s 1 ease forwards"
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    } else if (n < 1) {
+        slideIndex = slides.length
+    } else {
+        slideIndex = n
+    }
+    slides[slideIndex - 1].getElementsByClassName("slideshow-image")[0].style.animation = "rotateIn 2s 1 ease forwards"
+    slides[slideIndex - 1].style.animation = "inLeft 2s 1 ease forwards"
+}
+
+function printData(n){
+  alert(slides[n].style.animation)
+  alert(slides[n].style.visibility)
+  alert(slides[n].style.position)
 }
