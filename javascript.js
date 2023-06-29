7004a27b18b83c7112956f4e72aebb07d2e74116
stcksmsh <vukicevickosta@gmail.com>
2023-06-30 01:45:15 +0200

    changed skills div, mostly

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
-const skills = ['C', 'C++', 'Python', 'TensorFlow', 'Java', 'JavaScript', 'RISC-V', 'NodeJS', 'React']
+const skills = ['Java', 'C', 'C++',
+    'Python', 'TensorFlow', 'JavaScript',
+    'RISC-V', 'NodeJS', 'React'
+]
 
 const skillWrapper = document.getElementById('skills')
-let rows = (skills.length + 2) / 3;
+let rows = ~~((skills.length + 2) / 3);
 skillWrapper.style.gridTemplateRows = 'repeat(' + rows.toString() + ', minmax(0, 1fr))';
 
 for (var i = 0; i < skills.length; i++) {
@@ -16,10 +19,10 @@ for (var i = 0; i < skills.length; i++) {
 }
 
 let slideIndex = 1;
-showSlides(1)
+showSlides(1);
 
 function plusSlides(n) {
-  showSlides(slideIndex + n);
+    showSlides(slideIndex + n);
 }
 
 function currentSlide(n) {
@@ -27,28 +30,30 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if(n >= slideIndex){ /// to the right
-      if(n > slides.length){
-        n %= slides.length
-      }
-      slides[slideIndex - 1].getElementsByClassName("slideshow-image")[0].style.animation = "rotateOutRight 2s 1 ease forwards"
-      slides[slideIndex - 1].style.animation = "outRight 2s 1 ease forwards"
-      
-      slides[n - 1].getElementsByClassName("slideshow-image")[0].style.animation = "rotateInLeft 2s 1 ease forwards"
-      slides[n - 1].style.animation = "inLeft 2s 1 ease forwards"
-    }else{ /// to the left
-      if(n < 1){
-        n += slides.length
-      }
-      slides[slideIndex - 1].getElementsByClassName("slideshow-image")[0].style.animation = "rotateOutLeft 2s 1 ease forwards"
-      slides[slideIndex - 1].style.animation = "outLeft 2s 1 ease forwards"
-      
-      slides[n - 1].getElementsByClassName("slideshow-image")[0].style.animation = "rotateInRight 2s 1 ease forwards"
-      slides[n - 1].style.animation = "inRight 2s 1 ease forwards"
-    }
+    if (n >= slideIndex) { /// to the right
+        if (n > slides.length) {
+            n %= slides.length
+        }
+        dots[slideIndex - 1].style.backgroundColor = "#bbb";
+        slides[slideIndex - 1].getElementsByClassName("slideshow-image")[0].style.animation = "rotateOutRight 2s 1 ease forwards"
+        slides[slideIndex - 1].style.animation = "outRight 2s 1 ease forwards"
 
+        dots[n - 1].style.backgroundColor = "#717171";
+        slides[n - 1].getElementsByClassName("slideshow-image")[0].style.animation = "rotateInLeft 2s 1 ease forwards"
+        slides[n - 1].style.animation = "inLeft 2s 1 ease forwards"
+    } else { /// to the left
+        if (n < 1) {
+            n += slides.length
+        }
+        dots[slideIndex - 1].style.backgroundColor = "#bbb";
+        slides[slideIndex - 1].getElementsByClassName("slideshow-image")[0].style.animation = "rotateOutLeft 2s 1 ease forwards"
+        slides[slideIndex - 1].style.animation = "outLeft 2s 1 ease forwards"
+
+        dots[n - 1].style.backgroundColor = "#717171";
+        slides[n - 1].getElementsByClassName("slideshow-image")[0].style.animation = "rotateInRight 2s 1 ease forwards"
+        slides[n - 1].style.animation = "inRight 2s 1 ease forwards"
+    }
     slideIndex = n;
 }
