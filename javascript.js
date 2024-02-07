46fe59131733d62271b72434e41e38f6b124aac3
stcksmsh <vukicevickosta@gmail.com>
2024-02-07 06:58:37 +0100

    Added EmojiType Changed the slideshow system, now to add a new slide or rearange existing ones all I need to do is change two arrays in the javascript.js file

--- javascript.js
+++ javascript.js
@@ -18,6 +18,61 @@ for (var i = 0; i < skills.length; i++) {
     skillWrapper.appendChild(div);
 }
 
+
+const slideNames = ['13S112OS1', 'EmojiType', 'BeogradPlusPlus', 'JSOpeN', 'OSCalc'];
+const slideDescriptions = ['RISC-V project kernel for the course Operating Systems 1',
+'EmojiType - Emojis on the fly',
+'BeogradPlusPlus - Pointing out flaws in the public transport ticket system',
+'JSOpeN - Lightweight JSON parser for Java',
+'OSCalc - Simple spreadsheet program'];
+
+const slideshowContainer = document.getElementsByClassName("slideshow-container")[0];
+if(slideshowContainer == undefined) {
+    console.log('slideshowContainer is undefined');
+}
+for (var i = slideNames.length - 1; i >= 0; i--) {
+    let div = document.createElement('div');
+    div.classList = ['mySlides'];
+    let innerDiv = document.createElement('div');
+    innerDiv.classList = ['numbertext'];
+    innerDiv.innerHTML = (i + 1).toString() + ' / ' + (slideNames.length).toString();
+    div.appendChild(innerDiv);
+    let a1 = document.createElement('a');
+    a1.href = slideNames[i];
+    let img = document.createElement('img');
+    img.src = './images/' + slideNames[i] + '.png';
+    img.classList = ['slideshow-image'];
+    a1.appendChild(img);
+    div.appendChild(a1);
+    div.appendChild(document.createElement('br'));
+    div.appendChild(document.createElement('br'));
+    let a2 = document.createElement('a');
+    a2.href = slideNames[i];
+    let adiv = document.createElement('div');
+    adiv.classList = ['text'];
+    adiv.innerHTML = slideDescriptions[i];
+    a2.appendChild(adiv);
+    div.appendChild(a2);
+    // slideshowContainer.appendChild(div);   
+    slideshowContainer.prepend(div);
+}
+
+let div = document.createElement('div');
+div.style.textAlign = 'center';
+div.appendChild(document.createElement('br'));
+for (var i = 0; i < slideNames.length; i++) {
+    let span = document.createElement('span');
+    span.classList = ['dot'];
+    span.onclick = function() {
+        currentSlide(i);
+    }
+    div.appendChild(span);
+}
+slideshowContainer.appendChild(div);
+
+
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
 let slideIndex = 1;
 showSlides(1);
 
@@ -30,8 +85,6 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
     if (n >= slideIndex) { /// to the right
         if (n > slides.length) {
             n %= slides.length
